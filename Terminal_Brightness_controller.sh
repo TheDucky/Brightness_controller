ece8287df7891358d68c5d97dc831d636087d018
TheDucky <90206823+TheDucky@users.noreply.github.com>
2021-09-07 14:44:32 +0530

    Update Terminal_Brightness_controller.sh

--- Terminal_Brightness_controller.sh
+++ Terminal_Brightness_controller.sh
@@ -1,6 +1,10 @@
 #! /bin/bash
 
+echo "Getting the Monitor’s name"
+sleep 2s
+screen=$(xrandr | grep " connected" | cut -f1 -d " ")
 read -p "enter brightness volume from 0.5 to 3 : " b_lev
-xrandr | grep " connected" | cut -f1 -d " "
-read -p "to confirm action please type the above value : " screen
+echo "Changing the brightness level"
+sleep 2s
 xrandr --output $screen --brightness $b_lev
+echo "task complete"
