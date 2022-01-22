ab0b2b3f184fd5b155396a9aff2f7f1872a55e8a
Septillihedron <resha3adnandaru@gmail.com>
2022-01-22 15:42:14 +0000

    added python check

--- Zenity.sh
+++ Zenity.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-zenity --entry \
+brightness=$(zenity --entry \
 --title="Brightness Controller!" \
 --text="Enter Brightness level form 0.3 to 3.0:" \
---entry-text "1.0"
-echo $?
+--entry-text "1.0")
 
-python3 TheIf.py
+echo "print('true' if (0.3<=$brightness<=3) else 'false')" > TheIf.py
+
+if $(python3 TheIf.py)
+    then sh xrandr.sh $brightness
+    else echo "error"
+fi
+
+rm TheIf.py
--- xrandr.sh
+++ xrandr.sh
@@ -1,13 +1,10 @@
 #! /bin/bash
 
 echo "Getting the Monitor Info"
-sleep 2s
 screen=$(xrandr | grep " connected" | cut -f1 -d " ")
-read -p "enter brightness volume from 0.3 to 3 : " b_lev
 echo "Changing the brightness level"
-sleep 2s
-xrandr --output $screen --brightness $b_lev
-echo "task complete"
+xrandr --output $screen --brightness $1
+echo "Task complete"
 
 # xrandr | grep " connected" | cut -f1 -d " "
 # eDP-1 ---- LVDS-1
